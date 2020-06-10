c281736c0afe9e828732d386b296040587254bfb
Corrado <corradoct@hotmail.it>
2020-06-10 18:25:43 +0200

    Ho aggiunto le funzionalità jQuery anche per gli altri li

--- js/script.js
+++ js/script.js
@@ -1,13 +1,33 @@
 $(document).ready(
   function() {
+    // Quando il mouse entra nell'area dell'li apro il menù a tendina e inverto la freccia dell'li delle lingue.
     $('.withDropdown').mouseenter(
       function() {
-        $('.dropdown').addClass('displayFlex');
+        $(this).children('.dropdown').addClass('displayFlex');
+        if ($('i').hasClass('fa-angle-down')) {
+          $('i').removeClass('fa-angle-down');
+          $('i').addClass('fa-angle-up');
+        }
       }
     )
+    // Quando il mouse esce dall'area dell'li chiudo il menù a tendina e inverto la freccia dell'li delle lingue.
     $('.withDropdown').mouseleave(
       function() {
-        $('.dropdown').removeClass('displayFlex');
+        $(this).children('.dropdown').removeClass('displayFlex');
+        if ($('i').hasClass('fa-angle-up')) {
+          $('i').removeClass('fa-angle-up');
+          $('i').addClass('fa-angle-down');
+        }
+      }
+    )
+    // Quando clicco sugli li chiudo il menù a tendina e inverto la freccia dell'li delle lingue.
+    $('.withDropdown').click(
+      function() {
+        $(this).children('.dropdown').removeClass('displayFlex');
+        if ($('i').hasClass('fa-angle-up')) {
+          $('i').removeClass('fa-angle-up');
+          $('i').addClass('fa-angle-down');
+        }
       }
     )
   }
